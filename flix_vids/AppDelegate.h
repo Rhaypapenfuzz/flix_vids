832381998b66e0bb85ca5269b7ac8f0f9e5b921b
Raymond Gyabeng <rhaypapenfuzz@fb.com>
2019-06-26 11:15:12 -0700

    Initial Commit

---
+++ flix_vids/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flix_vids
+//
+//  Created by rhaypapenfuzz on 6/26/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flix_vids/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flix_vids
+//
+//  Created by rhaypapenfuzz on 6/26/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
